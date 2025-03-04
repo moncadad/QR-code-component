216672d73aeb130ffd6406e106acd4a1606d29a2
Denise <88467086+moncadad@users.noreply.github.com>
2025-03-04 16:19:14 -0600

    Update Info.jsx
    semantic correction to 
    - div now section
    - h2 now h1

--- src/componenets/Info.jsx
+++ src/componenets/Info.jsx
@@ -1,11 +1,11 @@
 export default function Info() {
   return (
-    <div className="qr--info">
-      <h2>Improve your front-end skills by building projects</h2>
+    <section className="qr--info">
+      <h1>Improve your front-end skills by building projects</h1>
       <p>
         Scan the QR code to visit Frontend Mentor and take your coding skils to
         the next level.
       </p>
-    </div>
+    </section>
   );
 }
